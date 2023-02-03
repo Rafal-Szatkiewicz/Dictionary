fd2eba54732b6ca8af1fd4f3bab3fc768962f42c
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-03 14:18:02 +0100

    errors handling

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const client = Owlbot(process.env.OWL_TOKEN);
 
-client.define('owl').then(function(result){
-   console.log(result);
+client.define('owl').then((result) => console.log(result)).catch(function (error) {
+   console.log('Error:', error.message);
 });

bf76fbc6fea10e92d0808cc0c2adc0164c524302
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-03 17:27:59 +0100

    test

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const axios = require('axios');
 const Owlbot = require('owlbot-js');
 require("dotenv").config();
 
+const test = document.getElementById("test");
 
 const client = Owlbot(process.env.OWL_TOKEN);
 
 client.define('owl')
-  .then(result => console.log(result))
+  .then(result => {
+    console.log(result);
+    test.innerHTML = result.definitions[0].type;
+  })
   .catch(error => console.error(`Error: ${error.message}`));
 
 axios({

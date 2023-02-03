95ee2d48d7314165996ebdd22ece61fa621e9cfd
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-03 14:19:58 +0100

    cleaner code

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require("dotenv").config();
 
 const client = Owlbot(process.env.OWL_TOKEN);
 
-client.define('owl').then((result) => console.log(result)).catch(function (error) {
-   console.log('Error:', error.message);
-});
+client.define('owl')
+  .then(result => console.log(result))
+  .catch(error => console.error(`Error: ${error.message}`));

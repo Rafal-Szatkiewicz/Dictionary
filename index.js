068807f0a5856ab2d2461e253e70431c20b1f73f
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-03 14:46:00 +0100

    added synonyms api

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+const axios = require('axios');
 const Owlbot = require('owlbot-js');
+const fs = require('fs');
 require("dotenv").config();
+
 
 const client = Owlbot(process.env.OWL_TOKEN);
 
 client.define('owl')
   .then(result => console.log(result))
   .catch(error => console.error(`Error: ${error.message}`));
+
+  axios({
+   method: "get",
+   url: `https://api.api-ninjas.com/v1/thesaurus?word=owl`,
+   headers: {
+      'X-Api-Key': process.env.SYNONYMS_TOKEN,
+   },
+ }).then((response) => {
+   console.log(response.data);
+ });

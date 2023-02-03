08b837a7b0d21e00b20d3b76b49e906b25b80d51
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-03 13:53:24 +0100

    axios uninstalled

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const axios = require('axios');
 const Owlbot = require('owlbot-js');
+require("dotenv").config();
 
-const client = Owlbot(YOUR_TOKEN);
+const client = Owlbot(process.env.OWL_TOKEN);
 
 client.define('owl').then(function(result){
    console.log(result);

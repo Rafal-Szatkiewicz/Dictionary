e5c55c19381821fe9df164ae3a2363a86502dfa9
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-06 23:10:42 +0100

    working prototype

--- public/api/product.js
+++ public/api/product.js
@@ -18,6 +18,8 @@ let search = "";
 router.get('/', async (req, res) => {
 
   const file = await fs.promises.readFile(path.join(__dirname, './index.html'), 'utf8');
+
+
   if(search != "")
   {
     let exists = true;
@@ -27,9 +29,32 @@ router.get('/', async (req, res) => {
       const definition = await axios({
         method: "get",
         url: `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
-      });
-      console.log("%j", definition.data);
-      res.json(definition.data)
+      }).catch(console.log("error"));
+      //console.log("%j", definition.data);
+      let html = `
+      <div class="result" id="result">
+      <div class="word">
+              <h3>${search}</h3>
+              <button onclick="playSound()">
+                  <i class="fas fa-volume-up"></i>
+              </button>
+          </div>
+          <div class="details">
+              <p>${definition.data[0].meanings[0].partOfSpeech}</p>
+              <p>/${definition.data[0].phonetic}/</p>
+          </div>
+          <p class="word-meaning">
+             ${definition.data[0].meanings[0].definitions[0].definition}
+          </p>
+          <p class="word-example">
+              ${definition.data[0].meanings[0].definitions[0].example || ""}
+          </p>
+          </div>`;
+    
+      let output = file.replace(' <div class="result" id="result">', html);
+      output = output.replace('<audio id="sound"></audio>', `<audio id="sound" src="${definition.data[0].phonetics[1].audio}"></audio>`)
+      //res.json(definition.data)
+      res.send(output);
     }
   }
   else

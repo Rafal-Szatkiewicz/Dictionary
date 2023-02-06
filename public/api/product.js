ba67fef44bd8e634f1f480b6ced1a7d8c7b15cb2
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-06 23:59:04 +0100

    error handling

--- public/api/product.js
+++ public/api/product.js
@@ -17,27 +17,30 @@ let search = "";
 
 router.get('/', async (req, res) => {
 
-  const file = await fs.promises.readFile(path.join(__dirname, './index.html'), 'utf8');
+  let file = await fs.promises.readFile(path.join(__dirname, './index.html'), 'utf8');
 
 
   if(search != "")
   {
     let exists = true;
 
+    const definition = await axios({
+      method: "get",
+      url: `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
+    }).catch(error => {
+      console.error(`Error: ${error.message}`); 
+      exists = false;
+    });
+
     if(exists)
     {
-      const definition = await axios({
-        method: "get",
-        url: `https://api.dictionaryapi.dev/api/v2/entries/en/${search}`
-      }).catch(console.log("error"));
+      
       //console.log("%j", definition.data);
       let html = `
       <div class="result" id="result">
       <div class="word">
               <h3>${search}</h3>
-              <button onclick="playSound()">
-                  <i class="fas fa-volume-up"></i>
-              </button>
+              <button onclick="playSound()"><i class="fas fa-volume-up"></i></button>
           </div>
           <div class="details">
               <p>${definition.data[0].meanings[0].partOfSpeech}</p>
@@ -51,14 +54,29 @@ router.get('/', async (req, res) => {
           </p>
           </div>`;
     
-      let output = file.replace(' <div class="result" id="result">', html);
-      output = output.replace('<audio id="sound"></audio>', `<audio id="sound" src="${definition.data[0].phonetics[1].audio}"></audio>`)
+      file = file.replace('<div class="result" id="result">', html);
+      if (definition.data[0].phonetics[1] && typeof definition.data[0].phonetics[1].audio !== 'undefined') 
+      {
+        file = file.replace('<audio id="sound"></audio>', `<audio id="sound" src="${definition.data[0].phonetics[1].audio}"></audio>`);
+      } 
+      else 
+      {
+          file = file.replace('<audio id="sound"></audio>', "");
+          file = file.replace(`<i class="fas fa-volume-up"></i>`, "");
+          file = file.replace(`<button onclick="playSound()"></button>`, "");
+      }
       //res.json(definition.data)
-      res.send(output);
+      res.send(file);
+    }
+    else
+    {
+      file = file.replace('<div class="result" id="result">', `<div class="result" id="result"><h3 class="error">Couldn't Find The Word</h3>`);
+      res.send(file);
     }
   }
   else
   {
+    file = file.replace('<audio id="sound"></audio>', "");
     res.send(file);
   }
 });

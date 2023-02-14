41cd4ece5ae71b948ade38dd0f541bc9eb5e3e1e
rafal-szatkiewicz <rafalszatkiewicz2@gmail.com>
2023-02-14 18:03:31 +0100

    fixed copyright date

--- public/api/product.js
+++ public/api/product.js
@@ -124,9 +124,9 @@ router.get('/', async (req, res) => {
           </div>
           ${details}
           </div>
-          <div id="copyright"><p><i>© 2021 Rafał Szatkiewicz</i></p></div>`;
+          <div id="copyright"><p><i>© 2023 Rafał Szatkiewicz</i></p></div>`;
     
-      file = file.replace('<div id="copyright"><p><i>© 2021 Rafał Szatkiewicz</i></p></div>', "");
+      file = file.replace('<div id="copyright"><p><i>© 2023 Rafał Szatkiewicz</i></p></div>', "");
       file = file.replace('<div class="result" id="result">', html);
       file = file.replace('height: 100vh;transition: 1.5s;','height: 100%;transition: 1.5s;');
       //res.json(definition.data)
